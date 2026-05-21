45aeba28f5a99ffc8360b40c830ab6db264bbdec
giuseppebalducci <iccudlabeppesuig@gmail.com>
2026-05-21 14:30:06 +0200

    sostituisce Exercise1 e Exercise2 di Vincenzo con i miei

--- src/main/java/org/generation/italy/examples/arrays/Exercise1.java
+++ src/main/java/org/generation/italy/examples/arrays/Exercise1.java
@@ -1,29 +1,36 @@
 package org.generation.italy.examples.arrays;
-/* dato un array di 10 elementi di qualsiasi tipo, invertire l'ordine degli elementi*/
-
 
 public class Exercise1 {
-    static void main() {
-        int[]  numArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] temp = new int[ numArray.length];
+    // dato un array di 10 elementi, di qualunque tipo, invertire l'ordine degli elementi
+    void main() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        // il modo più semplice per farlo è creare un altro array e copiarci gli elementi
+        // al contrario, poi ricopiare il nuovo array nel primo.
+        // in questo caso non è necessario, teniamoci l'array temporaneo.
 
-        for (int i = 0; i< numArray.length ; i++){
-            temp[ numArray.length-1-i] =   numArray[i];
+        // inizializziamo l'array temp. in questo modo si riempie di 0
+        int[] temp = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
+            temp[arr.length - 1 - i] = arr[i];
         }
-
-        System.out.println("\nL'indirizzo dell'array temporaneo è: \n" + temp + "\nL'indirizzo dell'array originale è: \n" + numArray);
-        System.out.println("\nEcco il risultato dell'esercizio:");
+        System.out.println(temp); // this prints the memory address pointing to the array, not the array
         printArray(temp);
     }
 
-    static void printArray(int [] array) {
-        for (int x : array) {
-            System.out.println(x);
-        }
-
-        /* for (int i = 0; i<a.length ; i++){
+    static void printArray(int[] a){   // this is what Array.toString() does
+        for (int i = 0; i < a.length; i++){
             System.out.println(a[i]);
-        } */
-    }
+        }
+        /*
+        we could write this with a for-each.
+        this way, the variable j itself gets filled
+        with the contents of the array, but
+        we won't have a counter,
+        which we may need depending on the case.
 
+        for (int j : a) {
+            System.out.println(j);
+        }
+        */
+    }
 }
--- src/main/java/org/generation/italy/examples/arrays/Exercise2.java
+++ src/main/java/org/generation/italy/examples/arrays/Exercise2.java
@@ -1,22 +1,22 @@
 package org.generation.italy.examples.arrays;
-
 import java.util.Arrays;
 
-// scrivere funzione che popola array con numeri interi casuali da 1 a 10
-
 public class Exercise2 {
-    static void main(){
-        int[]  array= new int[10];
-
+    // given an array of integers, write a method that fills it
+    // with random integers in the range 0-10
+    void main() {
+        int[] array = new int[10]; // fills with 10 zeroes
         populateArray(array);
         IO.println(Arrays.toString(array));
     }
 
-    static void populateArray(int[] array) {
-            for(int i=0;i<array.length;i++){
-            array[i]=(int)(Math.random()*10)+1;
+    static void populateArray(int[] inArray) {
+        for (int i=0; i<inArray.length; i++) {
+            // Math.random() generates from 0.0 inclusive to 1.0 exclusive. 0.0 - 0.99
+            // we multiply * 10 cause by only casting, we would only have 0! cause of truncation
+            // +1 inside the parentheses would have worked, but we'd have one more casting operation
+            // so it's better to write it outside.
+            inArray[i] = (int)(Math.random() * 10) + 1;
         }
-
     }
-
 }
