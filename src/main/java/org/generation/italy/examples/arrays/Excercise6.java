d1384caa40579b198a1eefaf895dc8ff36d76a73
Chiara De Santis <chiara.desantis@virgilio.it>
2026-05-21 18:05:33 +0200

    Esercizi con gli array

---
+++ src/main/java/org/generation/italy/examples/arrays/Excercise6.java
@@ -0,0 +1,45 @@
+package org.generation.italy.examples.arrays;
+
+//Dato un array di 11 String di cui 10 vuote e 1 di valore “U” (inizialmente all’indice 5), simulare il problema del cammino dell’ubriaco:
+//A ogni passo l’ubriaco “U” si sposta casualmente verso sinistra o verso destra di 1 posizione nell’array. L’ubriaco continua a spostarsi finché non esce dall’array.
+//
+//Ad ogni passo, stampare la visualizzazione grafica di tutto l’array per mostrare la posizione attuale dell’ubriaco.
+//
+//Il programma non deve crashare quando l’ubriaco sta per uscire dall’array.
+//
+//Alla fine dell’esecuzione, stampare il numero di passi che l’ubriaco ha compiuto prima di uscire.
+//
+//
+//int randomInt = (int)(Math.random() * (max - min + 1)) + min;**
+
+
+import java.util.Arrays;
+
+public class Excercise6 {
+    static void main() {
+        String[] drunkArr = new String[11];
+        drunkArr[5] = "U";
+
+        int maxVal = +1;
+        int minVal = -1;
+
+        int drunkPosition = 5;
+
+        int count = 0;
+
+
+        while(drunkPosition >= 0 && drunkPosition <= 10){
+            int randomInt = (int)(Math.random() * (maxVal - minVal + 1)) + minVal;
+            drunkArr[drunkPosition] = null; // svuota la vecchia posizione
+            drunkPosition = drunkPosition + randomInt;
+
+            if(drunkPosition >= 0 && drunkPosition <= 10){
+                drunkArr[drunkPosition] = "U";
+            }
+
+            count++;
+            IO.println(Arrays.toString(drunkArr));
+        }
+        IO.println("Passi: " + count);
+    }
+}
---
+++ src/main/java/org/generation/italy/examples/arrays/Excercise7.java
@@ -0,0 +1,39 @@
+package org.generation.italy.examples.arrays;
+
+// una funzione dato un array di interi  torna il numero ripetuto più volte. voglio solo un occorrenza se 2 numeri si ripetono più volte.
+
+import java.util.Arrays;
+
+public class Excercise7 {
+    static void main() {
+        int[] arr = new int[10];
+
+        int[] countArr = new int[11];
+        int maxCount = arr[0];
+        int maxNumber = 0;
+        String result = "";
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = (int)(Math.random() * 10) + 1;
+
+            countArr[arr[i]]++;
+
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(countArr[i] > maxCount){
+                maxCount = countArr[i];
+                maxNumber = i;
+            }
+        }
+        for(int i = 0; i < countArr.length; i++){
+            if(countArr[i] == maxCount){
+                result = result + i + " ";
+            }
+
+        }
+        IO.println(Arrays.toString(arr));
+        IO.println(result);
+
+
+    }
+}
