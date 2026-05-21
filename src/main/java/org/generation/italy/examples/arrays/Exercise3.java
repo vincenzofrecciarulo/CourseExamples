fd2c4cbe24d5d37013db11da35709ebf66712d26
giuseppebalducci <iccudlabeppesuig@gmail.com>
2026-05-21 15:20:12 +0200

    aggiunge Exercise3 e Exercise4

---
+++ src/main/java/org/generation/italy/examples/arrays/Exercise3.java
@@ -0,0 +1,28 @@
+package org.generation.italy.examples.arrays;
+
+import java.util.Arrays;
+
+public class Exercise3 {
+    // scriviamo una funzione che riceve in input un array e lo inverte
+    // direttamente, senza creare array temporanei
+    void main() {
+        int[] numArray = {1, 3, 2, 6, 8, 9, 7, 4, 0};
+        IO.println(Arrays.toString(numArray));
+        invert(numArray);
+        IO.println(Arrays.toString(numArray));
+    }
+
+    public static void invert(int[] numArray) {
+        // we use two counters, one starting from 0, the other from arraylength-1
+        for (int i=0, j=numArray.length-1; i < j; i++, j--) {
+            swap(numArray, i, j);
+        }
+    }
+
+    public static void swap(int[] numArray, int i, int j) {
+        // we swap the two integers with the 3rd temp variable technique we already seen
+        int temp = numArray[i];
+        numArray[i] = numArray[j];
+        numArray[j] = temp;
+    }
+}
---
+++ src/test/java/org/generation/italy/examples/arrays/Exercise3Test.java
@@ -0,0 +1,37 @@
+package org.generation.italy.examples.arrays;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Exercise3Test {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void invert_test() {
+        int[] arr = {0, 3, 4, 6, 9, 2, 15, 18, 29, 784, 32};
+        Exercise3.invert(arr);
+        int[] expected = {32, 784, 29, 18, 15, 2, 9, 6, 4, 3, 0};
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    void has_unique_numbers_returns_true_for_unique_numbers() {
+        int[] numbersTest = {1, 2, 3, 4, 5, 6, 7};
+        boolean distinct = Exercise4.hasUniqueNumbers(numbersTest);
+        assertTrue(distinct);
+    }
+
+    @Test
+    void has_unique_numbers_returns_false_for_duplicates() {
+        int[] numbersTest = {1, 2, 3, 4, 5, 6, 6};
+        boolean distinct = Exercise4.hasUniqueNumbers(numbersTest);
+        assertFalse(distinct);
+    }
+}
