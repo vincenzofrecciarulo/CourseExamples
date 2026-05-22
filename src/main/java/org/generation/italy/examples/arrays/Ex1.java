13e3c8173247cbb0c05d0ada38a648fb4d233feb
Trylo <huamansalazarl006@gmail.com>
2026-05-22 09:11:40 +0200

    Ho creato una classe per iniziare gli esercizi

--- src/main/java/org/generation/italy/examples/arrays/Ex1.java
+++ src/main/java/org/generation/italy/examples/arrays/Ex1.java
@@ -2,6 +2,9 @@ package org.generation.italy.examples.arrays;
 
 public class Ex1 {
     static void main() {
+    int[] numeri = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for(int i = 0; i <= 10; i++){
 
+    }
     }
 }
