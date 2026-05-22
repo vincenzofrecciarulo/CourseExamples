87daca11572da40870708355e6c1a208e8c9c4ff
Domenico Piano <catry.piano@gmail.com>
2026-05-22 09:12:12 +0200

    Creato esercizio 5

---
+++ src/main/java/org/generation/italy/examples/arrays/Exercise5.java
@@ -0,0 +1,26 @@
+package org.generation.italy.examples.arrays;
+
+// Dato un array di 10 elementi di qualunque tipo, invertire l’ordine degli elementi
+
+public class Exercise5 {
+    static void main(){
+        int[] numArray = {1,2,3,4,5,6,7,8,9,10};
+
+        // Inversione array
+        for (int i = 0; i < numArray.length / 2; i++) {
+
+            int temp = numArray[i];
+
+            numArray[i] = numArray[numArray.length - 1 - i];
+
+
+            numArray[numArray.length - 1 - i] = temp;
+        }
+
+        // Stampa array invertito
+        for (int i = 0; i < numArray.length; i++) {
+
+            System.out.print(numArray[i] + " ");
+        }
+    }
+}
