c21d5dc19a1d5c9f9afc5be236b00175a898a174
Trylo <huamansalazarl006@gmail.com>
2026-05-22 12:33:51 +0200

    Implementa una prima versione del MagicHat procedurale.

--- src/main/java/org/generation/italy/examples/hogwarts/MagicHat.java
+++ src/main/java/org/generation/italy/examples/hogwarts/MagicHat.java
@@ -35,7 +35,7 @@ public class MagicHat {
             {"Giuseppe Balducci", House.RAVENCLAW.name()},
             {"Manuel Sechi", House.SLYTHERIN.name()},
             {"Chiara De Santis", House.GRYFFINDOR.name()},
-            {"Matteo De Cata", House.HUFFLEPUFF .name()},
+            {"Matteo De Cata", House.HUFFLEPUFF.name()},
     };
     /*  Abbiamo bisogno delle dimensioni di ogni casata, quindi numero di studenti diviso ogni casa non funziona correttamente.
     //  e ad ogni modo / 4 è una bad practice, quindi House.values().length mi serve a dare il numero da dividere, quindi 4.
@@ -45,26 +45,93 @@ public class MagicHat {
     //qui sotto
     final static int PERFECT_CLASS_SIZE = (PERFECT_HOUSE_SIZE) * House.values().length;
 
-    final static boolean EXTRA_STUDENTS = (students.length + prefects.length) != PERFECT_HOUSE_SIZE;
+    final static boolean EXTRA_STUDENTS = (students.length + prefects.length) != PERFECT_CLASS_SIZE;
 
     //      QUESTO è MOLTO IMPORTANTE PER DIVIDERE LE ALTRE PERSONE NELLE CASATE
-    final static String[][] houses = new String[House.values().length][EXTRA_STUDENTS ? PERFECT_CLASS_SIZE + 1 : PERFECT_CLASS_SIZE];
+    final static String[][] houses = new String[House.values().length][EXTRA_STUDENTS ? PERFECT_HOUSE_SIZE + 1 : PERFECT_HOUSE_SIZE];
 
     final static int[] counters = new int[House.values().length];
 
     final static Random luck = new Random();
 
-    public static void assignPrefects()
+    static void main() throws InterruptedException {                //una main che lancia una eccezione è assolutamente da EVITARE
+        shuffleStudents();
+        assignPrefects();
+        assignStudents();
+        assignExtraStudents();
+        reportAssignments();
+    }
 
-    static void shuffleStudents(){
+
+
+    static void assignPrefects() {
+        for (int i = 0; i < prefects.length; i++) {
+            String[] prefect = prefects[i];
+            String name = prefect[0];
+            String houseName = prefect[1];
+            House h = House.valueOf(houseName);             //  valueOf prende in input una stringa e restituisce il valore della stringa
+            int pos = h.ordinal();
+            houses[pos][0] = name;
+            counters[pos]++;
+        }
+    }
+
+    static void assignStudents() throws InterruptedException {
+        for (int i = 0; i < PERFECT_CLASS_SIZE - prefects.length; i++) {
+            String[] student = students[i];
+            String studentName = student[0];
+            String houseName = student[1];
+
+            House favoriteHouse = null;
+            if(houseName != null){
+                favoriteHouse = House.valueOf(houseName);
+            }
+
+            IO.println(studentName + " .......mmmhhhh..");
+            //Thread.sleep(2000 + luck.nextInt(2000));
+            if (houseName != null){
+                int favoriteHousePos = favoriteHouse.ordinal();
+
+                boolean hasSpace = counters[favoriteHousePos] < PERFECT_HOUSE_SIZE;
+
+                if (hasSpace) {
+                    int chance = luck.nextInt(3);
+                    if (chance == 0) {
+                        houses[favoriteHousePos][counters[favoriteHousePos]] = studentName;
+                        counters[favoriteHousePos]++;
+                        IO.println(studentName + " ha avuto culo ed è stato/a aggiunto/a alla sua casa preferita");
+                        continue;
+                    }
+                }
+            }
+
+            int randomPos;
+            do {
+                randomPos = luck.nextInt(houses.length);
+            } while (counters[randomPos] == PERFECT_HOUSE_SIZE);
+
+            houses[randomPos][counters[randomPos]] = studentName;
+            counters[randomPos]++;
+            String randomHouseName = House.values()[randomPos].name();
+            IO.println(studentName + " è stato/a aggiunto/a casualmente a " + randomHouseName);
+
+            //lazy evaluation: l'operatore && valuta in ordine la prima condizione, e poi la seconda.
+            if (favoriteHouse != null && randomHouseName.equals(favoriteHouse.name())) {
+                IO.println("Che botta di culo di " + studentName + " ad essere aggiunti alla propria casa preferita!! : "
+                        + favoriteHouse.name());
+            }
+        }
+    }
+
+    static void shuffleStudents() {
 
         for (int i = 0; i < 100; i++) {
 
             int p1 = luck.nextInt(students.length);     //  0 - students.length
             int p2;
-                                                        //lanciamo random finchè non escono diversi
-                                                        //una variabile scritta nelle graffe del do/while non verrà letta
-                                                        //in nessun altro posto.
+            //lanciamo random finchè non escono diversi
+            //una variabile scritta nelle graffe del do/while non verrà letta
+            //in nessun altro posto.
             do {
                 p2 = luck.nextInt(students.length);
             } while (p1 == p2);
@@ -73,6 +140,37 @@ public class MagicHat {
             String[] temp = students[p1];                  //variabile temporanea, stesso algoritmo ma con array di stringa
             students[p1] = students[p2];
             students[p2] = temp;
+        }
+    }
+
+    static void assignExtraStudents() {
+        for (int i = PERFECT_CLASS_SIZE - prefects.length; i < students.length; i++) {
+            int randomPos;
+            String[] student = students[i];
+            String studentName = student[0];
+            House favoriteHouse = House.valueOf(student[1]);
+            do {
+                randomPos = luck.nextInt(houses.length);
+            } while (counters[randomPos] == PERFECT_HOUSE_SIZE + 1);
+
+            houses[randomPos][counters[randomPos]] = studentName;
+            counters[randomPos]++;
+            String randomHouseName = House.values()[randomPos].name();
+            IO.println(studentName + " è stato/a aggiunto/a casualmente a " + randomHouseName);
+            if (randomHouseName.equals(favoriteHouse.name())) {
+                IO.println("Che botta di culo di " + studentName + " ad essere aggiunti alla propria casa preferita!! : "
+                        + favoriteHouse.name());
+            }
+        }
+    }
+
+    public static void reportAssignments(){
+        for(int i = 0; i < houses.length; i++){
+            IO.print(House.values()[i].name() + " ");
+            for(int j = 0; j < houses[0].length; j++){
+                IO.print(houses[i][j] + " ");
+            }
+            IO.println();
         }
     }
 }
