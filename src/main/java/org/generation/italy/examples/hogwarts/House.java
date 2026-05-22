339f8c7ec4b9521fc468b9d7de3fef03e1d9e2e3
giuseppebalducci <iccudlabeppesuig@gmail.com>
2026-05-22 10:25:37 +0200

    implementa parzialmente MagicHat

---
+++ src/main/java/org/generation/italy/examples/hogwarts/House.java
@@ -0,0 +1,12 @@
+package org.generation.italy.examples.hogwarts;
+
+public enum House {
+    /*
+    gli enum sono un modo per restringere il pool
+    di valori/costanti possibili nella classe a n valori da noi definiti.
+    per convenzione, i membri delle enum si scrivono in capslock.
+
+    definiamo un tipo House, che può contenere solo questi 4 valori
+    */
+    GRYFFINDOR, HUFFLEPUFF, RAVENCLAW, SLYTHERIN
+}
