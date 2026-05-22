4a5fe3f16aa3bbcb175e685b8fa120eacaf91df4
aless <alessandrodn01@gmail.com>
2026-05-22 02:30:17 +0200

    aggiunge l'esercizio dell'ubriaco e il test degli step

--- src/test/java/org/generation/italy/examples/arrays/Exercise2TrisTest.java
+++ src/test/java/org/generation/italy/examples/arrays/Exercise2TrisTest.java
@@ -16,11 +16,9 @@ class Exercise2TrisTest {
 
         @Test
         void test_findDuplicates() {
-            // Input con duplicati: 3 e 7
             int[] input = {3, 7, 1, 3, 9, 10, 7, 2, 4, 5};
 
-            // Aspettativa: array di 10 elementi con 1 agli indici 2 (per il 3) e 6 (per il 7)
-            int[] expected = {0, 0, 1, 0, 0, 0, 1, 0, 0, 0};
+            int[] expected = {1, 1, 2, 1, 1, 0, 2, 0, 1, 1};
 
             int[] result = Exercise2Tris.rollCall(input);
 
