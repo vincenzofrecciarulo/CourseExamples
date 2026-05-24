8dffade9ab5e7c0bc778de523767db9f9b7063e8
Matteo_DeCata <mecata99@gmail.com>
2026-05-24 18:49:47 +0200

    finito veramente esercizio 3

--- src/main/java/org/generation/italy/examples/arrays/casa/riccardo/Exercise3.java
+++ src/main/java/org/generation/italy/examples/arrays/casa/riccardo/Exercise3.java
@@ -3,15 +3,21 @@ package org.generation.italy.examples.arrays.casa.riccardo;
 // Scrivi una funzione che riceve in input due stringhe e un carattere
 // e ritorna true se la prima stringa contiene quel carattere un numero di volte superiore alla seconda
 
+// Ho dovuto utilizzare i comandi "split" e "charAt" anche se non li abbiamo visti a lezione
+// però li ho utilizzati solo nel main e non nella funzione
+
 public class Exercise3 {
     public static void main(String[] args){
         String firstWord = IO.readln("Inserisci la prima parola: ");
-        String[] firstArrayStr = firstWord.split("");
+        String[] firstArrayStr = firstWord.split(""); // "split" converte una stringa in un vettore stringa. In questo caso divide la stringa su ogni carattere vuoto tra le lettere, ottenendo esattamente un elemento per ogni carattere
 
         String secondWord = IO.readln("Inserisci la seconda parola: ");
         String[] secondArrayStr = secondWord.split("");
 
-        char character = IO.readln("Inserisci un carattere: ").charAt(0);
+        String characterStr = IO.readln("Inserisci un carattere: ")
+        char character = characterStr.charAt(0); // "readln" restituisce sempre una stringa.
+                                                 // Il comando "charAt" converte quella stringa in un carattere.
+                                                 // In questo caso, "charAt(0)" restituisce la prima posizione della stringa convertita
 
         if (checkCharacter(firstArrayStr, secondArrayStr, character)){
             System.out.println("La prima stringa contiene il carattere " + character + " più volte della seconda stringa.");
