f955b49e1fe7f632345b3a107f44639aa655d141
Matteo_DeCata <mecata99@gmail.com>
2026-05-24 21:59:49 +0200

    iniziato ora esercizio 7

--- src/main/java/org/generation/italy/examples/arrays/casa/riccardo/Exercise7.java
+++ src/main/java/org/generation/italy/examples/arrays/casa/riccardo/Exercise7.java
@@ -1,4 +1,8 @@
 package org.generation.italy.examples.arrays.casa.riccardo;
 
+// Scrivi una funzione che riceve in input un numero n
+// e ritorna un vettore bidimensionale (matrice) n x n
+// in cui tutti i valori sono zero tranne quelli sulla diagonale
+
 public class Exercise7 {
 }
