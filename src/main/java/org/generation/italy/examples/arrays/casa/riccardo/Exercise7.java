8f659f0fed7e2581a8ad5e41b24ccb3233e95213
Matteo_DeCata <mecata99@gmail.com>
2026-05-25 00:20:12 +0200

    esercizio 7 finito

--- src/main/java/org/generation/italy/examples/arrays/casa/riccardo/Exercise7.java
+++ src/main/java/org/generation/italy/examples/arrays/casa/riccardo/Exercise7.java
@@ -1,23 +1,52 @@
 package org.generation.italy.examples.arrays.casa.riccardo;
 
+import java.util.Random;
 // Scrivi una funzione che riceve in input un numero n
 // e ritorna un vettore bidimensionale (matrice) n x n
 // in cui tutti i valori sono zero tranne quelli sulla diagonale
 
 public class Exercise7 {
     public static void main(String[] args){
+        String numberStr = IO.readln("Inserisci un numero intero maggiore o uguale a 1 per definire la dimensione della matrice: ");
+        int number = Integer.parseInt(numberStr);
+
+        if(number>=1){
+            int[][] bidMatrix = returnMatrix(number);
+            System.out.println("La matrice è: ");
+            printMatrix(bidMatrix);
+        }else{
+            System.out.println("Il numero inserito non è valido!");
+            System.out.println();
+        }
 
     }
 
     public static int[][] returnMatrix (int num){
+
+        Random randomNum = new Random();
+
         int[][] matrix = new int[num][num];
 
         for(int i=0; i<num; i++){
             for(int j=0; j<num; j++){
                 if (i==j){
-                    matrix[i][j] ==
+                    matrix[i][j] = randomNum.nextInt(10) + 1; // con quel "+1" ci escono i numeri da 1  10, così differenziamo la diagonale da tutti gli altri zeri nella matrice
+                }else{
+                    matrix[i][j] = 0;
                 }
             }
+        }
+
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
