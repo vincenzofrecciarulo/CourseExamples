2e92dbd2dc6add60620377fc324634dcc9650b09
Domenico Piano <catry.piano@gmail.com>
2026-05-25 14:02:33 +0200

    Creato esercizio 7 e 8

---
+++ src/main/java/org/generation/italy/examples/arrays/Exercise7.java
@@ -0,0 +1,48 @@
+/*
+Ex3
+Dato un array di 10 elementi, scrivere una funzione che popola l’array con numeri interi casuali di valore (1) - (100),
+invocarla sull’array e stampare il valore massimo e il valore minimo
+
+Ex3Bis (solitaria)
+Rifarlo ma con valori (-100) - (+100)
+ */
+
+package org.generation.italy.examples.arrays;
+
+import java.util.Arrays;
+
+public class Exercise7 {
+    static void main() {
+        int[] array = new int[10];
+        popolaArray(array, -100, 100);
+        System.out.println(Arrays.toString(array));
+        stampaMinimoMassimo(array);
+    }
+
+    static int generaNumeriCasuali(int a, int b){
+        if (a >= b) {
+            throw new IllegalArgumentException("Il parametro a deve essere inferiore al parametro b"); //lancia eccezione che interrompe programma con messagio di errore
+        }
+        double number = Math.random();
+        return (int) Math.floor(number*(b-a+1))+a; //math.floor arrotonda per diffetto.
+    }
+    static void popolaArray(int[] array,int a, int b){
+        for(int i = 0; i < array.length; i++){
+            array[i] = generaNumeriCasuali(a, b);
+        }
+    }
+    static void stampaMinimoMassimo(int[]array){
+        assert array.length > 0;               //l'array dev'essere maggiore di 0, oppure errore.
+        int min = array[0];
+        int max = array[0];
+        for(int i = 0; i< array.length; i++){
+            if (array[i] < min){
+                min = array[i];
+            }
+            if (array[i] > max){
+                max = array[i];
+            }
+        }
+        System.out.println("Il numero massimo è: "+max+"\nMentre il minimo è: "+min);
+    }
+}
---
+++ src/main/java/org/generation/italy/examples/arrays/Exercise8.java
@@ -0,0 +1,58 @@
+/*
+Ex4
+Dato un array di 10 elementi, scrivere una funzione che popola l’array con double casuali di valore (0) - (100),
+invocarla sull’array e stamparne la media matematica
+
+Ex4Bis (solitaria)
+Rifarlo ma calcolando la media senza il valore massimo e il valore minimo
+
+Ex4Tris (solitaria)
+Popolare l’array e stamparne solo gli elementi che distano più di 10 dalla media di tutto l’array
+*/
+
+package org.generation.italy.examples.arrays;
+
+import java.util.Arrays;
+
+public class Exercise8 {
+    static void main(String[] args) {
+        double[] array = new double[10];
+        popolaArray(array, 0, 100);
+        System.out.println(Arrays.toString(array));
+        System.out.println("Il valore medio dell'array è: "+calcolaMedia(array));
+        System.out.println("I numeri distanti più di 10 dalla media sono: ");
+        stampaValoriDistanti(array, 10);
+    }
+
+    static double generaNumeriCasuali(double a, double b){
+        if (a >= b) {
+            throw new IllegalArgumentException("Il parametro a deve essere inferiore al parametro b"); //lancia eccezione che interrompe programma con messagio di errore
+        }
+        double number = Math.random()*(b-a)+a;
+        return number;
+    }
+    static void popolaArray(double[] array,double a, double b){
+        for(int i = 0; i < array.length; i++){
+            array[i] = generaNumeriCasuali(a, b);
+        }
+    }
+    static double calcolaMedia(double[]array){
+        assert array.length > 0;  //l'array dev'essere maggiore di 0, oppure errore.
+        double sum = 0;
+        for (int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        double media = sum / (double)array.length;
+        return media;
+    }
+    static void stampaValoriDistanti(double[]array, double distanza){
+        assert array.length > 0;
+        assert distanza >= 0;
+        double media = calcolaMedia(array);
+        for (int i = 0; i < array.length; i++){
+            if (Math.abs(array[i] - media) > distanza){
+                System.out.println(array[i]);
+            }
+        }
+    }
+}
