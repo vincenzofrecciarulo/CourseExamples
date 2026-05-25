7acfe09e03b59f668eb39d69d849b54eeca25437
andrea-ferraro <andrea.ferraro.42@gmail.com>
2026-05-25 14:16:39 +0200

    Svolge gli esercizi su funzioni e array

---
+++ src/main/java/org/generation/italy/examples/riccardoExercises/Exercise1.java
@@ -0,0 +1,15 @@
+package org.generation.italy.examples.riccardoExercises;
+
+public class Exercise1 {
+    static void main() {
+        /*
+         * Scrivi una funzione main che crea due variabili intere ,
+         *  poi una che rappresenta la somma delle due e stampa il risultato.*/
+
+        int a = 10;
+        int b = 20;
+        int c = a +b;
+        System.out.println("Somma = "+c);
+    }
+
+}
