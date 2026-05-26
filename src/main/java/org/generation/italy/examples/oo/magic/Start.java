f0613dca93f7283a87565d83e88788ba9b2191b9
Chiara De Santis <chiara.desantis@virgilio.it>
2026-05-26 16:22:24 +0200

    Implementa la OOP nel progetto MagicHat

---
+++ src/main/java/org/generation/italy/examples/oo/magic/Start.java
@@ -0,0 +1,9 @@
+package org.generation.italy.examples.oo.magic;
+
+public class Start {
+
+    static void main() {
+        MagicHat hat = new MagicHat();
+        hat.startAssingmentCeremony();
+    }
+}
---
+++ src/main/java/org/generation/italy/examples/oo/magic/Student.java
@@ -0,0 +1,14 @@
+package org.generation.italy.examples.oo.magic;
+
+public class Student {
+
+    public String name;
+    public House favoriteHouse;
+    public House destinationHouse;
+
+
+    public Student(String name, House favoriteHouse) {
+        this.name = name;
+        this.favoriteHouse = favoriteHouse;
+    }
+}
