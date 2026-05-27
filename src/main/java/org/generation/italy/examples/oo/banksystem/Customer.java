91cc4c18fa11cdb4b758fd4754dff22930182307
Marko-00 <marcotamigi00@gmail.com>
2026-05-27 18:07:19 +0200

    Continua a mostrare altri esempi di incapsulamento e programmazione oo

---
+++ src/main/java/org/generation/italy/examples/oo/banksystem/Customer.java
@@ -0,0 +1,59 @@
+package org.generation.italy.examples.oo.banksystem;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Customer {
+    private String name;
+    private String surname;
+    private LocalDate dob;
+    private boolean female;
+    private ArrayList<Account> accounts;
+
+    public Customer(String name, String surname, LocalDate dob, boolean female, ArrayList<Account> accounts) {
+        this.name = name;
+        this.surname = surname;
+        this.dob = dob;
+        this.female = female;
+        this.accounts = accounts;
+    }
+    //questo costruttore invoca il costruttore in alto
+    public Customer(String name, String surname, LocalDate dob, boolean female) {
+        this(name, surname, dob, female, new ArrayList<>());
+    }
+    public Customer(String name, String surname, LocalDate dob){
+        this(name, surname, dob, true);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public boolean isFemale() {
+        return female;
+    }
+
+    public double getSpecialAccountBalance(){
+        double balance = 0;
+        if(female){
+            for(Account a : accounts){
+                if(a.getBalance() > balance){
+                    balance = a.getBalance();
+                }
+            }
+        }else{
+            for(Account a : accounts){
+
+            }
+        }
+        return balance;
+    }
+}
