9edc5b121879059017c7561e872652eddbfe5923
VincenzoFrecciarulo <vfrecciarulo@gmail.com>
2026-05-27 23:30:59 +0200

    un comando per getSpecial

--- src/main/java/org/generation/italy/examples/oo/banksystem/Customer.java
+++ src/main/java/org/generation/italy/examples/oo/banksystem/Customer.java
@@ -23,21 +23,28 @@ public class Customer {
         this(name, surname, dob, female, new ArrayList<>());    //Invoca il costruttore in alto, il programma lo capisce dagli argomenti che gli passa, così prende tutto che gli è passato dalla classe esterna e poi gli genera un ArrayList vuoto.
     }
 
+    public Customer(String name, String surname, LocalDate dob){
+        this(name,surname,dob,false);                   //'Per creare questa catena ogni costruttore deve passare all'altro sopra di esso il valore mancante, come vogliamo che sia di default
+    }
+
+    public Customer(String name, String surname) {
+        this(name, surname, null);
+    }
+
+    public Customer(String name){
+        this(name, "");
+    }
+
+
+
     public double getSpecialAccountBalance(){      //ritorna la balance più alta tra gli account se il customer è female o bassa se il customer è maschio
-        double balance = 0;
-        if (female) {
+        double balance = accounts.get(0).getBalance();
+        int maxMin = female ? 1 : -1;
             for (Account i : accounts) {                    // Vedere come fare per crearli tramite funzione che scorre e va bene per entrambi
-                if (i.getBalance()>balance) {
+                if (i.getBalance()*maxMin>balance*maxMin) {
                     balance = i.getBalance();
                 }
             }
-            balance = balance;
-        } else {
-            for (Account i : accounts) {
-
-            }
-
-        }
         return balance;
     }
 
