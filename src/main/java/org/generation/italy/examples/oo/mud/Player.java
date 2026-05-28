e7939c78b275ae8271a69323b05a7239afa4e1d0
Francesco Liberati <francesco.liberati99@gmail.com>
2026-05-28 22:53:03 +0200

    crea la classe Player del MUD

---
+++ src/main/java/org/generation/italy/examples/oo/mud/Player.java
@@ -0,0 +1,49 @@
+package org.generation.italy.examples.oo.mud;
+
+import java.util.ArrayList;
+
+public class Player extends Entity{
+
+    private int minDamage;
+    private int maxDamage;
+    private ArrayList<Item> inventory;
+
+    public Player(String name, String type){
+        super(100,name,1);
+        switch(type.toLowerCase()){
+            case "comandante":
+                this.minDamage=10;
+                this.maxDamage=20;
+                break;
+
+            case "guerriero":
+                this.minDamage=5;
+                this.maxDamage=12;
+                break;
+
+            default:
+                IO.println("sei un ubriacone della locanda");
+                this.minDamage=2;
+                this.maxDamage=6;
+                break;
+        }
+        this.inventory = new ArrayList<Item>();
+    }
+
+    public int calculateDamage(){
+        int damage = (int)(Math.random() * (this.maxDamage - this.minDamage + 1)) + this.minDamage;
+        return damage;
+    }
+
+    public void pickUpItem(Item item) {
+        this.inventory.add(item);
+        System.out.println("Hai raccolto: " + item.getName());
+    }
+
+    public void dropItem(Item item) {
+        this.inventory.remove(item);
+        System.out.println("Hai lasciato cadere: " + item.getName());
+    }
+
+
+}
