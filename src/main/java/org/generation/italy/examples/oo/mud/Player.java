7d1772e2c068eb21e297e689d4bf1fc5b0a1f9ed
Marko-00 <marcotamigi00@gmail.com>
2026-05-30 00:42:22 +0200

    aggiunge la classe Player al mud

---
+++ src/main/java/org/generation/italy/examples/oo/mud/Player.java
@@ -0,0 +1,42 @@
+package org.generation.italy.examples.oo.mud;
+
+public class Player {
+    private String name;
+    private int hp = 20;
+    private int atk = 10;
+    private int def = 8;
+    private int exp = 0;
+    private int level = 1;
+
+
+    public void gainExp(int exp){
+        this.exp += exp;
+        checkLevelUp();
+    }
+    public void checkLevelUp(){
+        if(exp == level * 20){
+            levelUp();
+        }
+    }
+    public void levelUp(){
+        level++;
+        hp += 2;
+        atk += 3;
+        def += 2;
+    }
+    public boolean isAlive(){
+        if(hp > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void getStats(){
+        IO.println("Statistiche del giocatore:");
+        IO.println("HP: " + hp);
+        IO.println("ATK: " + atk);
+        IO.println("DEF: " + def);
+        IO.println("LIVELLO: " + level);
+    }
+
+}
