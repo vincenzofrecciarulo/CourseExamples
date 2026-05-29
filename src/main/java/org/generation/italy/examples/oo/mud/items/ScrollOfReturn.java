c4d667092a85edd662b6ab470cc5ff97dcb32e39
Wen Jun Zheng Giacomo <wenjunzheng2001@gmail.com>
2026-05-29 21:27:32 +0200

    aggiunge logica per lo scroll del ritorno

---
+++ src/main/java/org/generation/italy/examples/oo/mud/items/ScrollOfReturn.java
@@ -0,0 +1,19 @@
+package org.generation.italy.examples.oo.mud.items;
+
+import org.generation.italy.examples.oo.mud.World;
+import org.generation.italy.examples.oo.mud.entities.Player;
+
+public class ScrollOfReturn extends Item {
+    private static final String NAME = "Scroll of return";
+    private static final int VALUE = 0;
+    private static final double WEIGHT = 5.0;
+
+    public ScrollOfReturn() {
+        super(ScrollOfReturn.WEIGHT, ScrollOfReturn.VALUE, ScrollOfReturn.NAME);
+    }
+
+    @Override
+    public void interact(Player player){
+        player.setCurrentRoom(World.start);
+    }
+}
