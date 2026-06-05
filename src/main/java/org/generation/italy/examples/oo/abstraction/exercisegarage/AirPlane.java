806a1364ec0400c1e31544f70facac9b8f70486b
andrea-ferraro <andrea.ferraro.42@gmail.com>
2026-06-05 12:04:36 +0200

    Implementazioni classi Garage,Car,Motorcycle e interfaccie Vehicle, FlyingVehicle

---
+++ src/main/java/org/generation/italy/examples/oo/abstraction/exercisegarage/AirPlane.java
@@ -0,0 +1,26 @@
+package org.generation.italy.examples.oo.abstraction.exercisegarage;
+
+public class AirPlane implements FlyingVehicle{
+
+
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public double payForVehicle() {
+        return 0;
+    }
+
+    @Override
+    public void turnOffEngine() {
+
+    }
+
+    @Override
+    public String getPlate() {
+        return "";
+    }
+}
--- src/main/java/org/generation/italy/examples/oo/mud/world/MudMain.java
+++
@@ -1,9 +0,0 @@
-package org.generation.italy.examples.oo.mud.world;
-
-public class MudMain {
-    public static void main(String[] args){
-        World w = new World();
-        w.startGame();
-    }
-}
-
