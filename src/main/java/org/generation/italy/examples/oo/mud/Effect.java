f2e83d73b7c797cce9dac6ad23aa5f53f4cd5646
andrea-ferraro <andrea.ferraro.42@gmail.com>
2026-06-08 18:48:02 +0200

    Riavvio Intellij

--- src/main/java/org/generation/italy/examples/oo/mud/Effect.java
+++
@@ -1,7 +0,0 @@
-package org.generation.italy.examples.oo.mud;
-
-public enum Effect {
-    HEAL,       // ripristina HP
-    ANTIDOTE,   // rimuove veleno
-    BOOST_ATK   // bonus temporaneo attacco
-}
