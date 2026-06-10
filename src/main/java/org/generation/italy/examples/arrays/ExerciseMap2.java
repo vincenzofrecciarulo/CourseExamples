f52f150e63286a94902d41e0ab7690a41bb97ad8
Francesco Liberati <francesco.liberati99@gmail.com>
2026-06-10 09:14:06 +0200

    implementa i commenti per l'inizio degli esercizi

---
+++ src/main/java/org/generation/italy/examples/arrays/ExerciseMap2.java
@@ -0,0 +1,22 @@
+package org.generation.italy.examples.arrays;
+
+public class ExerciseMap2 {
+    // esercizio 2:
+// creare una classe Employee.
+// un Employee ha un ID, nome, cognome, sesso e stipendio.
+// voglio una classe GestioneImpiegati (EmployeeManagement)
+// che abbia i seguenti metodi:
+// un metodo getById() che mi restituisce un Employee quando do in input il suo ID
+// dentro EmployeeManagement devono essere mantenuti n impiegati (10 nel nostro caso)
+// getById() dovrà avere efficienza O(1) - HashMap
+// voglio poi un altro metodo getAllEmployeesOrderedByAge, che mi restituisca tutti
+// gli Employee ordinati per età decrescente (dai più vecchi ai più giovani)
+
+// voglio poi un altro metodo che mi restituisca i due Employee con il salario
+// più alto
+
+// voglio poi un altro metodo che ritorni tutti gli impiegati, ordinati per cognome crescente
+// il cognome deve essere case insensitive.
+// siccome potremmo avere più impiegati con lo stesso cognome, a parità di cognome voglio che
+// abbiamo precedenza nel sort le donne rispetto agli uomini.
+}
