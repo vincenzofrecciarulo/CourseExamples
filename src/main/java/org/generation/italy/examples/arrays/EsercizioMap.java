1f1b28679125fd14e9ccf04f12bfed0ba01b96a9
Wen Jun Zheng Giacomo <wenjunzheng2001@gmail.com>
2026-06-10 22:55:56 +0200

    aggiunge esercizio sulle map

---
+++ src/main/java/org/generation/italy/examples/arrays/EsercizioMap.java
@@ -0,0 +1,32 @@
+package org.generation.italy.examples.arrays;
+
+public class EsercizioMap {
+     /*
+     // esercizio 1:
+    // creare una funzione statica che riceva in input
+    // un array di stringhe, e restituisca la moda
+    // di queste stringhe (l'elemento che appare più volte)
+    // l'algoritmo deve avere efficienza O(n)
+    // se ci sono più mode, ne ritorna una a caso
+
+    // esercizio 2:
+    // creare una classe Employee.
+    // un Employee ha un ID, nome, cognome, sesso e stipendio.
+    // voglio una classe GestioneImpiegati (EmployeeManagement)
+    // che abbia i seguenti metodi:
+    // un metodo getById() che mi restituisce un Employee quando do in input il suo ID
+    // dentro EmployeeManagement devono essere mantenuti n impiegati (10 nel nostro caso)
+    // getById() dovrà avere efficienza O(1) - HashMap
+    // voglio poi un altro metodo getAllEmployeesOrderedByAge, che mi restituisca tutti
+
+    // gli Employee ordinati per età decrescente (dai più vecchi ai più giovani)
+
+    // voglio poi un altro metodo che mi restituisca i due Employee con il salario
+    // più alto
+
+    // voglio poi un altro metodo che ritorni tutti gli impiegati, ordinati per cognome crescente
+    // il cognome deve essere case insensitive.
+    // siccome potremmo avere più impiegati con lo stesso cognome, a parità di cognome voglio che
+    // abbiamo precedenza nel sort le donne rispetto agli uomini.
+    */
+}
