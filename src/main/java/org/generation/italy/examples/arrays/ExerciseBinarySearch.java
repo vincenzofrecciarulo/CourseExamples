5625bf084fd6832b491751a3d13792970f7997fa
Myriam_Spagnuolo <spagnuolomyriam@gmail.com>
2026-06-10 15:49:01 +0200

    aggiungo un esercizio

---
+++ src/main/java/org/generation/italy/examples/arrays/ExerciseBinarySearch.java
@@ -0,0 +1,16 @@
+package org.generation.italy.examples.arrays;
+
+public class ExerciseBinarySearch {
+    public static void main(String[] args){
+        int[] nums= {1,3,7,9,13,15,20,25,30,40,56,98,100,150,300};
+
+    }
+
+    public static int indexOf(int[]numbers, int target){
+        int start= 0;
+        int end= numbers.length -1;
+
+        return start;
+
+    }
+}
