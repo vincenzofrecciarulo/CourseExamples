0657c914a8f6c12ac2dc36fb7c79bb69c42ce67f
VincenzoFrecciarulo <vfrecciarulo@gmail.com>
2026-06-10 16:58:44 +0200

    Continuato con le eccezioni e try/catch

--- src/main/java/org/generation/italy/examples/oo/exceptions/ConsoleInterface.java
+++ src/main/java/org/generation/italy/examples/oo/exceptions/ConsoleInterface.java
@@ -1,29 +1,47 @@
 package org.generation.italy.examples.oo.exceptions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class ConsoleInterface {
     private StudentService service = new StudentService();
 
-    static void main() {            //STACK: main->console.registerStudent()->Stringhe in input etc.->service.registerStudent()->repository.addStudent()
+    static void main() {                //STACK: main->console.registerStudent()->Stringhe in input etc.->service.registerStudent()->repository.addStudent()
         ConsoleInterface c = new ConsoleInterface();
-        c.registerStudent();        //L'eccezione arriva qui, se non gestisce crasha il programma (termina la main) e la VM stampa il messaggio del throw
+        try {
+            c.registerStudent();        //L'eccezione arriva qui, se non gestisce crasha il programma (termina la main) e la VM stampa il messaggio del throw
+        } catch (Exception e) {
+            IO.println("Il file necessario per registrare lo studente non esiste");
+            IO.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
-    public void registerStudent(){
+    public void registerStudent() throws FileNotFoundException {
         String firstname = IO.readln("Dammi il nome dello studente");
         String lastname = IO.readln("Dammi il cognome dello studente");
         String birthDateString = IO.readln("Dammi la data di nascita");
-        LocalDate birthDate = LocalDate.parse(birthDateString);
-        Student s = new Student(3, firstname, lastname, birthDate);
+        LocalDate birthDate;
+        try {                                               //Eccezione unchecked, ma che conviene gestire in quanto dipende dall'input utente
+            birthDate = LocalDate.parse(birthDateString);
+        } catch(DateTimeParseException d){
+            birthDate = LocalDate.now();                    //Soluzione totalmente arbitraria a scopo d'esempio
+        }
+        Student s = new Student(0, firstname, lastname, birthDate);
         try {                                   //Faccio al suo interno operazioni che potrebbero lanciare eccezioni (anche se indirettamente, in catena di invocazioni)
             service.registerStudent(s);         //Se il metodo che ha chiamato non gestisce l'exception, arriva direttamente qui
             IO.println("Congratulazioni, hai salvato lo studente.");        //Se linea 20 ha successo, si finisce il blocco di try e si ignora il catch
-        } catch(IllegalArgumentException e){                                //Se 20 non ha successo, salta nel blocco di catch in cui gestiamo l'eccezione (DOBBIAMO PREVEDERE IL TIPO DI ECCEZIONE)
-            IO.println("Mi spiace, ma non siamo riusciti a inserire lo studente.");
+        } catch (
+                IllegalArgumentException e) {                                //Se 20 non ha successo, salta nel blocco di catch in cui gestiamo l'eccezione (DOBBIAMO PREVEDERE IL TIPO DI ECCEZIONE)
             IO.println(e.getMessage());
+        /*} catch(FileNotFoundException f){       //Si apre un catch per ogni possibile exception
+            IO.println(f.getMessage());
+        }*/
+
+
         }
-
-
     }
 }
--- src/main/java/org/generation/italy/examples/oo/exceptions/StudentService.java
+++ src/main/java/org/generation/italy/examples/oo/exceptions/StudentService.java
@@ -1,15 +1,19 @@
 package org.generation.italy.examples.oo.exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class StudentService {
     private StudentRepository studentRepo = new StudentRepository();
     private static long idGenerator = 4;
-    public void registerStudent(Student s){
+
+    public void registerStudent(Student s) throws FileNotFoundException {      //Dichiara la pericolosità nella sua firma così l'invocatore sa che è pericoloso
         //Qui il servizio fa delle operazioni necessarie prima di registrare uno studente
         if (s.getId()==0) {
             s.setId(idGenerator++);
         }
-        studentRepo.addStudent(s);   //Aggiunge lo studente, se gli torna un eccezione e non fa nulla, la funzione si blocca qui
-
+        studentRepo.addStudent(s);                                              //Aggiunge lo studente, se gli torna un eccezione e non fa nulla, la funzione si blocca qui
+        FileReader fr = new FileReader("nonesisto.txt");                //Apre un file, siccome può non esistere per colpa dell'ambiente, siamo obbligati a gestire l'eccezione
         // Mezza tonnellata di business logic successiva al salvataggio di Student
 
     }
