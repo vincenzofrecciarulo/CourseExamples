cf5561be1291bb55217a3a24b19d0e40a89bb6ab
KetchOxy <manuelsechi330@gmail.com>
2026-06-30 14:32:08 +0200

    esegue esercizi jdbc

--- src/main/java/org/generation/italy/examples/jdbc/JDBCFactionRepository.java
+++ src/main/java/org/generation/italy/examples/jdbc/JDBCFactionRepository.java
@@ -1,9 +1,6 @@
 package org.generation.italy.examples.jdbc;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -23,6 +20,18 @@ public class JDBCFactionRepository implements FactionRepository{
             """
             SELECT id, name, description
             FROM faction
+            """;
+    private final static String GET_FACTION_BY_ID =
+            """
+            SELECT id, name, description
+            FROM faction
+            WHERE id = ?
+            """;
+    private final static String UPDATE_FACTION =
+            """
+            UPDATE faction
+            SET name = ?, description = ?
+            WHERE id = ?
             """;
     //anche se qua stiamo mettendo tutte le cose e potremmo quindi fare con * meglio
     //scriverli con l'ordine che vogliamo perché se si cambiassero poi le colonne
@@ -64,13 +73,35 @@ public class JDBCFactionRepository implements FactionRepository{
     }
 
     @Override
-    public Optional<Faction> getFactionByName(String name) throws DataException {
+    public Optional<Faction> getFactionById(int id) throws DataException {
+        try(PreparedStatement st = con.prepareStatement(GET_FACTION_BY_ID)){
+            st.setInt(1, id);
+            try(ResultSet rs = st.executeQuery()){
+                if(rs.next()){
+                    int factionId = rs.getInt("id");
+                    String name = rs.getString("name");
+                    String description = rs.getString("description");
+                    return Optional.of(new Faction(factionId, name, description));
+                }
+            }
+        }
+        catch (SQLException e){
+            throw new DataException(e.getMessage(),e);
+        }
         return Optional.empty();
     }
 
     @Override
     public boolean updateFaction(Faction faction) throws DataException {
-        return false;
+        try(PreparedStatement st = con.prepareStatement(UPDATE_FACTION)){
+            st.setString(1, faction.getName());
+            st.setString(2, faction.getDescription());
+            st.setInt(3, faction.getId());
+            return st.executeUpdate() > 0;
+        }
+        catch (SQLException e){
+            throw new DataException(e.getMessage(),e);
+        }
     }
 
     @Override
