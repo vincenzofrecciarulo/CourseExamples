84b124fb841461118064ad5f7e7407dc90015ba7
giuseppebalducci <iccudlabeppesuig@gmail.com>
2026-06-30 13:01:38 +0200

    implements some other JDBC concepts, adds Faction and FactionRepository

---
+++ src/main/java/org/generation/italy/examples/jdbc2/DataException.java
@@ -0,0 +1,18 @@
+package org.generation.italy.examples.jdbc2;
+
+// it should extend Exception, not RuntimeException,
+// cause we want this to be a checked exception.
+// when we will create this exception, we'll pass to it
+// an error message.
+
+// Exception constructor (which we call with super()) includes an Exception (or Throwable) cause
+// because it's made exactly for containing other Exceptions.
+
+// this way, we don't have to throw many different Exceptions.
+// we will ALWAYS throw DataException, and we will pass the original
+// exception (any kind) as its CAUSE.
+public class DataException extends Exception {
+    public DataException(String message, Exception cause) {
+        super(message, cause);
+    }
+}
---
+++ src/test/java/org/generation/italy/examples/jdbc2/JDBCFactionRepositoryTest.java
@@ -0,0 +1,37 @@
+package org.generation.italy.examples.jdbc2;
+
+import org.junit.jupiter.api.Test;
+import org.postgresql.core.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JDBCFactionRepositoryTest {
+    // we can't write proper unit tests for repositories
+    // these are technically integration tests, cause it's not only the class
+    // but how it integrates with the db.
+    // this is a subtlety.
+
+    @Test
+    void getAllFactions_throws_data_exception_containing_sql_exception() {
+        final String DB_URL = "jdbc:postgresql://localhost:5432/company";
+        final String DB_USER = "postgresMaster";
+        final String DB_PSW = "goPostgresGo";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PSW)) {
+            FactionRepository fr = new JDBCFactionRepository(connection);
+            // assert that when we execute the lambda an Exception will be thrown, of a type we say,
+            DataException de = assertThrows(DataException.class, fr::getAllFactions);
+            // assert that the DataException contains SQLException
+            SQLException se = assertInstanceOf(SQLException.class, de.getCause());
+            // we test the error message, it should be something like table does not exist
+            assertTrue(se.getMessage().contains("does not exist"));
+        }
+        catch (SQLException e) {
+            fail(e.getMessage()); // fail makes the test fail
+        }
+    }
+}
