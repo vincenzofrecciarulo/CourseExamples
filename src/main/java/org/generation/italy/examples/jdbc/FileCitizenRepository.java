ea6d7e76fc458c4b050661af889f28e2fc86168b
Domenico Piano <catry.piano@gmail.com>
2026-07-01 17:56:39 +0200

    Lavoro su JDBC con metodi

--- src/main/java/org/generation/italy/examples/jdbc/FileCitizenRepository.java
+++ src/main/java/org/generation/italy/examples/jdbc/FileCitizenRepository.java
@@ -41,7 +41,8 @@ public class FileCitizenRepository implements CitizenRepository{
 
     public List<Citizen> findAll2() throws DataException {
         try(Stream<String> lineStream = Files.lines(citizenFile.toPath())){
-           return lineStream.map(this::fromCsvLine).toList();
+           return lineStream.map(this::fromCsvLine)
+                            .toList();
         } catch (IOException e) {
             throw new DataException(e.getMessage(),e);
         }
@@ -49,17 +50,154 @@ public class FileCitizenRepository implements CitizenRepository{
 
     @Override
     public List<Citizen> findBySexAndEducationLevel(char sex, String educationLevel) throws DataException {
-        return List.of();
+        try(BufferedReader br = new BufferedReader(new FileReader(citizenFile))) {
+            List<Citizen> citizenList = new ArrayList<>();
+            String line = null;
+            while((line=br.readLine()) != null){
+                Citizen citizen = this.fromCsvLine(line);
+                if(citizen.getGender() == sex && citizen.getEducationLevel().equals(educationLevel)) {
+                    citizenList.add(citizen);
+                }
+            }
+            return citizenList;
+
+        }catch (IOException e){
+            throw new DataException(e.getMessage(), e);
+        }
+    }
+
+    public List<Citizen> findBySexAndEducationLevel2(char sex, String educationLevel) throws DataException {
+        try(Stream<String> lineStream = Files.lines(citizenFile.toPath())){
+            return lineStream.map(this::fromCsvLine)
+                             .filter(citizen -> citizen.getGender() == sex &&
+                                            citizen.getEducationLevel().equals(educationLevel))
+                             .toList();
+        } catch (IOException e) {
+            throw new DataException(e.getMessage(),e);
+        }
     }
 
     @Override
     public boolean updateCitizen(Citizen citizen) throws DataException {
-        return false;
+        List<Citizen> citizenList = this.findAll();
+        boolean updated = false;
+        for (int i = 0; i < citizenList.size(); i++) {
+            Citizen c = citizenList.get(i);
+            if (citizen.getId() == c.getId()){
+                citizenList.set(i, citizen);
+                updated = true;
+                break;
+            }
+        }
+        if (updated) {
+            try (FileWriter fw = new FileWriter(this.citizenFile, false)) {
+                for (Citizen c : citizenList) {
+                    String updatedCitizen = c.getId() + "," +
+                                            c.getFirstName() + "," +
+                                            c.getLastName() + "," +
+                                            c.getGender() + "," +
+                                            c.getAge() + "," +
+                                            c.getSalary() + "," +
+                                            c.getEducationLevel() + System.lineSeparator();
+                    fw.append(updatedCitizen);
+                }
+
+            } catch (IOException e) {
+                throw new DataException(e.getMessage(), e);
+            }
+        }
+        return updated;
+    }
+
+    public boolean updateCitizen2(Citizen citizen) throws DataException {
+        boolean updated = false;
+        try {
+            List<Citizen> original = this.findAll();
+                updated = original.stream()
+                                  .anyMatch(c -> c.getId() == citizen.getId());
+            List<Citizen> filtered = original.stream()
+                                             .map(c -> citizen.getId() == c.getId() ? citizen : c)
+                                             .toList();
+            if (updated) {
+                List<String> lines = filtered.stream()
+                        .map(c -> c.getId() + "," +
+                                c.getFirstName() + "," +
+                                c.getLastName() + "," +
+                                c.getGender() + "," +
+                                c.getAge() + "," +
+                                c.getSalary() + "," +
+                                c.getEducationLevel())
+                        .toList();
+
+
+                Files.write(citizenFile.toPath(), lines);
+            }
+
+        } catch (IOException e) {
+            throw new DataException(e.getMessage(), e);
+        }
+        return updated;
     }
 
     @Override
     public boolean deleteCitizen(int citizenId) throws DataException {
-        return false;
+        List<Citizen> citizenList = this.findAll();
+        boolean deleted = false;
+        for (int i = 0; i < citizenList.size(); i++) {
+            Citizen c = citizenList.get(i);
+            if (citizenId == c.getId()) {
+                citizenList.remove(c);
+                deleted = true;
+                break;
+            }
+        }
+        if (deleted) {
+            try (FileWriter fw = new FileWriter(this.citizenFile, false)) {
+                for (Citizen c : citizenList) {
+                    String updatedCitizen = c.getId() + "," +
+                                            c.getFirstName() + "," +
+                                            c.getLastName() + "," +
+                                            c.getGender() + "," +
+                                            c.getAge() + "," +
+                                            c.getSalary() + "," +
+                                            c.getEducationLevel() + System.lineSeparator();
+                    fw.append(updatedCitizen);
+                }
+
+            } catch (IOException e) {
+                throw new DataException(e.getMessage(), e);
+            }
+        }
+
+        return deleted;
+    }
+
+    public boolean deleteCitizen2(int citizenId) throws DataException {
+        boolean deleted;
+        try {
+            List<Citizen> original = this.findAll();
+            List<Citizen> filtered = original.stream()
+                    .filter(citizen -> citizen.getId() != citizenId)
+                    .toList();
+            deleted = filtered.size() < original.size();
+
+            if (deleted) {
+                List<String> lines = filtered.stream()
+                        .map(c -> c.getId() + "," +
+                                c.getFirstName() + "," +
+                                c.getLastName() + "," +
+                                c.getGender() + "," +
+                                c.getAge() + "," +
+                                c.getSalary() + "," +
+                                c.getEducationLevel())
+                        .toList();
+
+                Files.write(citizenFile.toPath(), lines);
+            }
+        } catch (IOException e) {
+            throw new DataException(e.getMessage(), e);
+        }
+        return deleted;
     }
 
     @Override
