ad7a87998c56bb3e56beaa897a340f2a6c211e7c
Jacopo De maio <jajodemaio@gmail.com>
2026-07-01 15:02:38 +0200

    implementazione non completa FileCitizenRepository

--- src/main/java/org/generation/italy/examples/jdbc/FileCitizenRepository.java
+++ src/main/java/org/generation/italy/examples/jdbc/FileCitizenRepository.java
@@ -1,15 +1,23 @@
 package org.generation.italy.examples.jdbc;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.SQLException;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.stream.Stream;
 
 // questa è l'implementazione di citizen repository che mantiene i dati su un file in formato CSV (Comma-Separated-Values)
 
 // csv -> tabella -> header(id, firstName..) - dati
 
 public class FileCitizenRepository implements CitizenRepository{
+
+    private File file;
+
+    public FileCitizenRepository(File file){
+        this.file = file;
+    }
 
     @Override
     public List<Citizen> findAll() throws DataException {
@@ -33,7 +41,25 @@ public class FileCitizenRepository implements CitizenRepository{
 
     @Override
     public Citizen createCitizen(Citizen newCitizen) throws DataException {
-        return null;
+        try(Stream<String> st = Files.lines(file.toPath())){
+            newCitizen = st.map(this::transformCitizenToLine).findFirst().get();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return newCitizen;
+    }
+
+    public Citizen transformCitizenToLine(String line){
+        String[] strgs = line.split(",");
+        return  new Citizen(
+                strgs[0],
+                strgs[1],
+                strgs[2].charAt(0),
+                Integer.parseInt(strgs[3]),
+                Double.parseDouble(strgs[4]),
+                strgs[5]
+        );
     }
 
     @Override
