2efb476e88e4aaf3a68750bfb31521a29f1c6484
Wen Jun Zheng Giacomo <wenjunzheng2001@gmail.com>
2026-07-02 11:18:38 +0200

    aggiunge altro test FileRepo

--- src/test/java/org/generation/italy/examples/jdbc/FileCitizenRepositoryTest.java
+++ src/test/java/org/generation/italy/examples/jdbc/FileCitizenRepositoryTest.java
@@ -1,30 +1,45 @@
 package org.generation.italy.examples.jdbc;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
+import java.nio.file.*;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class FileCitizenRepositoryTest {
     private Citizen c;
     private File citizenFile;
+    private static File originalFile;
     private FileCitizenRepository repo;
+
+
+    @BeforeAll
+    static void beforeAll() {
+        try {
+            Path originalPath = Path.of("data","test_citizens_original.csv");
+            Files.createDirectories(originalPath.getParent());
+            Files.writeString(originalPath,"", StandardOpenOption.CREATE);
+            originalFile = new File(originalPath.toUri());
+
+        }catch (IOException e){
+            IO.println("Qualcosa è rotto");
+        }
+    }
+
     @BeforeEach
     void setUp() {
-        c = new Citizen(4,"Lucia","Sperti",'F',33,
+        c = new Citizen(6,"Carlos","Sperti",'F',33,
                 1400,"College");
         Path path = Path.of("data","test_citizens.csv");
         try {
             Files.createDirectories(path.getParent());
             Files.writeString(path,"", StandardOpenOption.CREATE);
             citizenFile = new File(path.toUri());
+
+            Files.copy(originalFile.toPath(), citizenFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
             repo = new FileCitizenRepository(citizenFile);
         } catch (IOException e) {
             e.printStackTrace();
@@ -64,21 +79,42 @@ class FileCitizenRepositoryTest {
     }
 
     @Test
-    void updateCitizen() {
+    void updateCitizen_return_true() {
         try {
-            repo.updateCitizen(new Citizen(4,"Lucianna","Sperti",'F',33,
+            boolean result = repo.updateCitizen(new Citizen(4,"Lucianna","Sperti",'F',33,
                     1400,"College"));
-
+            assertTrue(result);
         } catch (DataException e) {
             fail(e.getMessage());
         }
     }
 
     @Test
-    void deleteCitizen() {
+    void updateCitizen_return_false() {
         try {
-            repo.deleteCitizen(2);
+            boolean result = repo.updateCitizen(new Citizen(27,"Lucianna","Sperti",'F',33,
+                    1400,"College"));
+            assertFalse(result);
+        } catch (DataException e) {
+            fail(e.getMessage());
+        }
+    }
 
+    @Test
+    void deleteCitizen_return_true() {
+        try {
+            boolean result = repo.deleteCitizen(2);
+            assertTrue(result);
+        } catch (DataException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void deleteCitizen_return_false() {
+        try {
+            boolean result = repo.deleteCitizen(29);
+            assertFalse(result);
         } catch (DataException e) {
             fail(e.getMessage());
         }
@@ -87,7 +123,8 @@ class FileCitizenRepositoryTest {
     @Test
     void createCitizen() {
         try {
-            repo.createCitizen(c);
+            Citizen citizen = repo.createCitizen(c);
+            assertEquals(c, citizen);
         } catch (DataException e) {
             fail(e.getMessage());
         }
