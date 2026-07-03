79dde7013e2274226dbc075285b5e9ad4702cf50
andrea-ferraro <andrea.ferraro.42@gmail.com>
2026-07-03 16:53:30 +0200

    Continuo a implementare JDBCCitizenRepository

---
+++ src/main/java/org/generation/italy/examples/tropico/TropicoCitizen.java
@@ -0,0 +1,12 @@
+package org.generation.italy.examples.tropico;
+
+public class TropicoCitizen {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+    private int age;
+    private String educationLevel;
+    private double salary;
+
+}
