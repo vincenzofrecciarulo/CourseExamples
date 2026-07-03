6949e453ace560976a6631852048635741e2aec3
aless <alessandrodn01@gmail.com>
2026-07-03 15:10:37 +0200

    implementa la repository di citizen per il csv

---
+++ src/main/java/org/generation/italy/examples/tropico/TropicoConsole.java
@@ -0,0 +1,7 @@
+package org.generation.italy.examples.tropico;
+//fara'da intefaccia utente via terminale
+public class TropicoConsole {
+    static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/generation/italy/examples/tropico/TropicoService.java
@@ -0,0 +1,4 @@
+package org.generation.italy.examples.tropico;
+// contenitore della logica di business della mia applicazione
+public class TropicoService {
+}
