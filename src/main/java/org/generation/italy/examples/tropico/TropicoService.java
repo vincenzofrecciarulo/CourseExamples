806bdb6c3afbfe7e939a0ced615a9bb9bf35e4f6
MyriamSpagnuolo <spagnuolomyriam@gmail.com>
2026-07-04 18:29:44 +0200

    crea la classe e finisce due metodi

--- src/main/java/org/generation/italy/examples/tropico/TropicoService.java
+++ src/main/java/org/generation/italy/examples/tropico/TropicoService.java
@@ -1,4 +1,44 @@
 package org.generation.italy.examples.tropico;
 
+import org.generation.italy.examples.jdbc.Citizen;
+import org.generation.italy.examples.jdbc.CitizenRepository;
+import org.generation.italy.examples.jdbc.DataException;
+
+import java.util.List;
+
 public class TropicoService {
+    private CitizenRepository cr;
+
+
+
+    public TropicoService(CitizenRepository cr) {
+        this.cr = cr;
+    }
+
+
+
+
+    public List<Citizen> findAllCitizens() throws DataException {
+    return cr.findAll();
+
+    }
+
+    public boolean deleteCitizen(int citizenId) throws DataException{
+    return cr.deleteCitizen(citizenId);
+    }
+
+    public boolean addCitizen (Citizen newCitizen) throws DataException{
+    return cr.createCitizen(newCitizen);
+    }
+
+
+    public List<Citizen> findAllBySexAndEducationLevel(char sex, String educationLevel) throws DataException{
+    return cr.findBySexAndEducationLevel(char sex, String educationLevel);
+    }
+
+    public boolean changeCitizenHappinessLevel (int citizenId, int happiness) throws DataException{
+
+    }
+
+
 }
