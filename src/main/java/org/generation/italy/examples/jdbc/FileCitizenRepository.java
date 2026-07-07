6485786fbd5ba67dfe1adff0245d42adc02300a8
KetchOxy <manuelsechi330@gmail.com>
2026-07-07 16:46:27 +0200

    esegue esercizi jdbc su file csv

--- src/main/java/org/generation/italy/examples/jdbc/FileCitizenRepository.java
+++ src/main/java/org/generation/italy/examples/jdbc/FileCitizenRepository.java
@@ -1,6 +1,6 @@
 package org.generation.italy.examples.jdbc;
 
-import org.generation.italy.examples.model.Citizen;
+import org.generation.italy.examples.model.tropico.Citizen;
 import java.awt.dnd.DropTarget;
 import java.io.*;
 import java.nio.file.Files;
