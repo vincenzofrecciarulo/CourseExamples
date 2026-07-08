dcf9da7a7797ae7fec966caa0e9a2ecfdd96e7ad
simone <bergerosimone@gmail.com>
2026-07-08 14:22:41 +0200

    esercizi su pattern factory

---
+++ src/main/java/org/generation/italy/examples/oo/patterns/factory/simplefactory/exercices/Start.java
@@ -0,0 +1,6 @@
+package org.generation.italy.examples.oo.patterns.factory.simplefactory.exercices;
+// assumendo di avere un'interfaccia notification che ha un solo metodo send di String message recipient
+// e tre sue implementazioni EmailNotification SMSNotification e WhatsAppNotification
+// voglio permettere la creazione in modo semplice e polimorfico di oggetti di questi tre tipi all'interno di una classe NotificationSender
+public class Start {
+}
